887d058f5c04d057c028560edbb8fdabcdb61962
Luis Roldan <hichi8520@gmail.com>
2021-09-21 16:50:25 -0600

    LZW Branch

---
+++ Library-LAB2/LZW.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Library_LAB2
+{
+    public class LZW : ICompressor
+    {
+        
+        public string Compress(string text)
+        {
+
+        }
+        public string Decompress(string text)
+        {
+
+        }
+        
+    }
+}
